0c8060461ce174b1aa2025a5d4cdfeefa8bee7de
ryanjacquess <r.jacques111@gmail.com>
2019-04-01 14:41:53 -0600

    Create autoComplete.h

---
+++ autoComplete.h
@@ -0,0 +1,26 @@
+#ifndef AUTO_COMPLETE_H
+#define AUTO_COMPLETE_H
+
+#include <memory>
+#include <string>
+#include <map>
+#include "iCommand.h"
+
+using namespace std;
+
+typedef map<string, unsigned int> autoMap;
+
+class AutoComplete {
+    private:
+        autoMap words;
+        autoMap temp;
+
+    public:
+        string search(string s);
+
+        void update(string s);
+
+        void del(string s);
+};
+
+#endif
