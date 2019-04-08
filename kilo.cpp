7c9604d631c55febeaacc59bca258cc9bdcaf423
ArthurChan0427 <44986059+ArthurChan0427@users.noreply.github.com>
2019-04-07 20:45:07 -0600

    Update kilo.cpp

--- kilo.cpp
+++ kilo.cpp
@@ -1,3 +1,12 @@
+// ------------------------------------------------------------------------------------
+//    Name:  Ryan Jacques         / Arthur Chan
+//    ID:    1523211              / 1425382
+//    Email: rjacques@ualberta.ca / yungjen@ualberta.ca
+//    CMPUT 275, Winter 2019
+//
+//    Final Project: Terminal Text Editor with Auto-Completion and Undo/Redo Features
+// ------------------------------------------------------------------------------------
+
 /*** includes ***/
 #define _DEFAULT_SOURCE
 #define _BSD_SOURCE
