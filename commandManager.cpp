565fbc0a4a0a4910e8058909e102685ff7b33b99
ArthurChan0427 <44986059+ArthurChan0427@users.noreply.github.com>
2019-04-07 22:13:29 -0600

    Update commandManager.cpp

--- commandManager.cpp
+++ commandManager.cpp
@@ -7,6 +7,8 @@
 //    Final Project: Terminal Text Editor with Auto-Completion and Undo/Redo Features
 // ------------------------------------------------------------------------------------
 
+// Source: http://gernotklingler.com/blog/implementing-undoredo-with-the-command-pattern/
+
 #include <memory>
 #include <stack>
 #include "commandManager.h"
