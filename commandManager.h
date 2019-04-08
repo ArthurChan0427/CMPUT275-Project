650219ca444fa4683129d8ff9ee9807d9322c35a
ArthurChan0427 <44986059+ArthurChan0427@users.noreply.github.com>
2019-04-07 20:45:35 -0600

    Update commandManager.h

--- commandManager.h
+++ commandManager.h
@@ -1,3 +1,12 @@
+// ------------------------------------------------------------------------------------
+//    Name:  Ryan Jacques         / Arthur Chan
+//    ID:    1523211              / 1425382
+//    Email: rjacques@ualberta.ca / yungjen@ualberta.ca
+//    CMPUT 275, Winter 2019
+//
+//    Final Project: Terminal Text Editor with Auto-Completion and Undo/Redo Features
+// ------------------------------------------------------------------------------------
+
 #ifndef COMMAND_MANAGER_H
 #define COMMAND_MANAGER_H
 
