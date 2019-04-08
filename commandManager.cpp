b8daa5bda9b6bb0c48ac371aab334bc448f0e2e1
ArthurChan0427 <44986059+ArthurChan0427@users.noreply.github.com>
2019-04-07 22:34:01 -0600

    Update commandManager.cpp

--- commandManager.cpp
+++ commandManager.cpp
@@ -17,21 +17,21 @@
 using namespace std;
 
 void CommandManager::executeCmd(shared_ptr<ICommand> command) {
-    redoStack = commandStack_t();
-    command->execute();
-    undoStack.push(command);
+    redoStack = commandStack_t(); // clear the redo-command stack
+    command->execute(); // execute the command
+    undoStack.push(command); // add the command object to the undo-command stack
 }
 
 void CommandManager::undo() {
     if (undoStack.size() <= 0) return;
-    undoStack.top()->undo();
-    redoStack.push(undoStack.top());
-    undoStack.pop();
+    undoStack.top()->undo(); // undo the most recent command
+    redoStack.push(undoStack.top()); // add the command object to the redo-command stack
+    undoStack.pop(); // remove the command object from the undo-command stack
 }
 
 void CommandManager::redo() {
     if (redoStack.size() <= 0) return;
-    redoStack.top()->redo();
-    undoStack.push(redoStack.top());
-    redoStack.pop();
+    redoStack.top()->redo(); // redo the most recent undone command
+    undoStack.push(redoStack.top()); // add the command object to the undo-command stack
+    redoStack.pop(); // remove the command object from the redo-command stack
 }
